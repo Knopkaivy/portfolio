a2c04a7df090c41c1818d2d9a6422b0466fc451c
Knopka <knopkaivy@gmail.com>
2022-10-24 17:17:30 -0500

    fill content for Contacts component

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import '../styles/Contacts.css';
 
 const Contacts = () => {
-  return <div className="Contacts">Contacts Page</div>;
+  return (
+    <div className="Contacts">
+      <h3 className="Contacts__subheader">Reach out to me at:</h3>
+      <ul className="Contacts__list">
+        <li className="Contacts__listItem">knokaivy@gmail.com</li>
+        <li className="Contacts__listItem">
+          linkedin.com/in/tetiana-ivy-213801134/
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Contacts;
