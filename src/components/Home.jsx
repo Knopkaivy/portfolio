199ddb2e5de55131a0a22431691f12180b980ff0
Knopka <knopkaivy@gmail.com>
2022-10-24 08:07:38 -0500

    fill content for Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import '../styles/Home.css';
 
 const Home = () => {
-  return <div className="Home">Home Page</div>;
+  return (
+    <div className="Home">
+      <p className="Home__greeting">Hi, my name is</p>
+      <h1 className="Home__header">Tetiana Ivy.</h1>
+      <h2 className="Home__subheader">I build things for the web.</h2>
+      <p className="Home__description">
+        I am a Front End Developer. I love woriking with CSS and am fasinated
+        with React JS. Currently I’m learning and discovering all things
+        development, and working on personal projects to apply these new skills.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
