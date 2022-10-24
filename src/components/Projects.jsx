acb43da36dfb84a56394a22afdd91bcd4b558ea8
Knopka <knopkaivy@gmail.com>
2022-10-24 17:43:04 -0500

    add starter file and project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import TodoList from '../images/todoList.JPG';
+import TravelIpsum from '../images/travelIpsum.JPG';
+import MakeAPalette from '../images/makeAPalette.JPG';
+import PhotographerPortfolio from '../images/photographerPortfolio.JPG';
 import '../styles/Projects.css';
 
 const Projects = () => {
---
+++ src/starter.js
@@ -0,0 +1,30 @@
+export const projectList = [
+  {
+    image: 'TodoList',
+    name: 'Todo List',
+    linkUrl: 'https://todo-list-7fc18.web.app/',
+    description: 'Todo list with authentication and cloud storage',
+    tools: ['HTML', 'CSS', 'React JS', 'Firebase'],
+  },
+  {
+    image: 'TravelIpsum',
+    name: 'Travel Ipsum',
+    linkUrl: 'https://travel-ipsum.web.app/',
+    description: 'Placeholder text for travel content',
+    tools: ['HTML', 'CSS', 'React JS'],
+  },
+  {
+    image: 'MakeAPalette',
+    name: 'Make A Palette',
+    linkUrl: 'https://make-a-palette-29fd5.web.app/',
+    description: 'Make a color palette from image url',
+    tools: ['HTML', 'CSS', 'React JS'],
+  },
+  {
+    image: 'PhotographerPortfolio',
+    name: 'Photographer Portfolio',
+    linkUrl: 'https://photographer-portfolio-demo.web.app/',
+    description: 'Sample portfolio site for photographer',
+    tools: ['HTML', 'CSS', 'React JS'],
+  },
+];
