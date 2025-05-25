3f4a48c6bfaef11670b653891965844cbb0112f7
TIVY-PK\tivy <tetiana.ivy@concentrix.com>
2025-05-25 12:23:23 -0500

    add borgo, brwBar and real estate listing projects to the list

--- src/starter.js
+++ src/starter.js
@@ -1,3 +1,6 @@
+import BorgoSantandrea from './images/borgoSantandrea.jpg';
+import EmiliaIngram from './images/emiliaIngram.jpg';
+import BrwBar from './images/brwBar.jpg';
 import TodoList from './images/todoList.JPG';
 import TravelIpsum from './images/travelIpsum.JPG';
 import MakeAPalette from './images/makeAPalette.JPG';
@@ -6,38 +9,60 @@ import ImageSearchAPI from './images/imageSearchAPI.jpg';
 
 export const projectList = [
   {
-    image: TodoList,
-    name: 'Todo List',
-    linkUrl: 'https://todo-list-7fc18.web.app/',
-    description: 'Todo list with authentication and cloud storage',
-    tools: 'HTML, CSS, React JS, Firebase',
+    image: BorgoSantandrea,
+    name: 'Hotel Home Page',
+    linkUrl: 'https://borgo-santandrea-home-demo.web.app/',
+    description: 'Sample hospitality business home page',
+    tools: 'HTML, SCSS, JavaScript, Swiper, ScrollTrigger',
   },
   {
-    image: TravelIpsum,
-    name: 'Travel Ipsum',
-    linkUrl: 'https://travel-ipsum.web.app/',
-    description: 'Placeholder text for travel content',
-    tools: 'HTML, CSS, React JS',
+    image: EmiliaIngram,
+    name: 'Real Estate Listing',
+    linkUrl: 'https://real-estate-landing-demo.web.app/',
+    description: 'Sample real estate listing website',
+    tools: 'HTML, CSS, ReactJS',
   },
   {
-    image: MakeAPalette,
-    name: 'Make A Palette',
-    linkUrl: 'https://make-a-palette-29fd5.web.app/',
-    description: 'Make a color palette from image url',
-    tools: 'HTML, CSS, React JS, localStorage',
+    image: BrwBar,
+    name: 'Beauty Salon Home Page',
+    linkUrl: 'https://beauty-salon-landing-demo.web.app/',
+    description: 'Sample esthetician business home page',
+    tools: 'HTML, CSS, JavaScript',
   },
   {
     image: PhotographerPortfolio,
     name: 'Photographer Portfolio',
     linkUrl: 'https://photographer-portfolio-demo.web.app/',
     description: 'Sample portfolio site for photographer',
-    tools: 'HTML, CSS, React JS',
+    tools: 'HTML, CSS, ReactJS',
   },
   {
     image: ImageSearchAPI,
     name: 'Image Search API',
     linkUrl: 'https://image-search-unsplash-api.web.app/',
     description: 'Unsplash API image search abstraction layer',
-    tools: 'HTML, CSS, React JS, API',
+    tools: 'HTML, CSS, ReactJS, API',
   },
+  {
+    image: TodoList,
+    name: 'Todo List',
+    linkUrl: 'https://todo-list-7fc18.web.app/',
+    description: 'Todo list with authentication and cloud storage',
+    tools: 'HTML, CSS, ReactJS, Firebase',
+  },
+  {
+    image: TravelIpsum,
+    name: 'Travel Ipsum',
+    linkUrl: 'https://travel-ipsum.web.app/',
+    description: 'Placeholder text for travel content',
+    tools: 'HTML, CSS, ReactJS',
+  },
+  {
+    image: MakeAPalette,
+    name: 'Make A Palette',
+    linkUrl: 'https://make-a-palette-29fd5.web.app/',
+    description: 'Make a color palette from image url',
+    tools: 'HTML, CSS, ReactJS, localStorage',
+  },
+
 ];
